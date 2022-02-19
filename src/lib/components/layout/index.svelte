95bd85480c6a56ae52781a7b0ed2253b00157b41
Agustinus Nathaniel <berserk.nate@gmail.com>
2022-02-19 14:13:05 +0700

    chore: switch to yarn, update layout and samples

--- src/lib/components/layout/index.svelte
+++ src/lib/components/layout/index.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import DefaultMetaTags from './DefaultMetaTags.svelte';
 	import Footer from './Footer.svelte';
-	import Header from './Header.svelte';
 </script>
 
 <DefaultMetaTags />
-<Header />
-<main class="layout px-4 py-8">
-	<slot />
-</main>
-<Footer />
+
+<div class="min-h-screen">
+	<main class="layout px-4 py-8">
+		<slot />
+	</main>
+	<Footer />
+</div>
